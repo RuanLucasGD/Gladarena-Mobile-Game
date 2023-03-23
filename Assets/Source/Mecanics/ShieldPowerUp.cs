ec6303760ba4cf780f533e6dbf3a7d23694f072e
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2023-03-23 14:49:53 -0400

    improv: recreate force shield behaviour

--- Assets/Source/Mecanics/ShieldPowerUp.cs
+++ Assets/Source/Mecanics/ShieldPowerUp.cs
@@ -6,29 +6,28 @@ namespace Game.Mecanics
 {
     public class ShieldPowerUp : PowerUp
     {
+        public float Damage;
+        public float Force;
+
         public override Character Owner
         {
             get => base.Owner;
             set
             {
-                // remove the power up effect of the character when change/remove your power up
-                if (Owner)
-                {
-                    Owner.IsInvencible = false;
-                }
-
-                // add power up effect on character
-                if (value)
-                {
-                    value.IsInvencible = true;
-                }
-
                 base.Owner = value;
-
                 gameObject.SetActive(Owner != null);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<Character>(out var c))
+            {
+                if (c != Owner)
+                {
+                    c.AddDamage(Damage, (c.transform.position - transform.position).normalized * Force);
+                }
             }
         }
     }
 }
-
-
