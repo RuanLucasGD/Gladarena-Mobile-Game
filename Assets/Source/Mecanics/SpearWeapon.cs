8f8886b302d0d06ecb9122d9e50e0233a1e19ca9
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2023-04-04 10:20:05 -0400

    implement: new spear soldier animated

--- Assets/Source/Mecanics/SpearWeapon.cs
+++ Assets/Source/Mecanics/SpearWeapon.cs
@@ -12,6 +12,8 @@ namespace Game.Mecanics
         [Header("Components")]
         public Collider WeaponCollider;
 
+        private List<Character> _attackedEnemies; // list ta be contain all attacked enemies of earch attack
+
         public SpearWeapon()
         {
             AttackForce = 15;
@@ -20,6 +22,8 @@ namespace Game.Mecanics
 
         protected override void Awake()
         {
+            _attackedEnemies = new List<Character>();
+
             base.Awake();
 
             OnEnableAttack += EnableWeaponCollider;
@@ -38,9 +42,18 @@ namespace Game.Mecanics
             // apply attack on detect target
             if (other.gameObject.TryGetComponent<Character>(out var character))
             {
-                if (character == WeaponTarget)
+                if (character == WeaponTarget)  // attack only one enemy
                 {
+                    foreach (var attackedEnemy in _attackedEnemies)
+                    {
+                        if (character == attackedEnemy) // don't attack enemy if is already attacked
+                        {
+                            return;
+                        }
+                    }
+
                     WeaponTarget.AddDamage(Damage, Owner.transform.forward * AttackForce);
+                    _attackedEnemies.Add(WeaponTarget);
                 }
             }
         }
@@ -108,6 +121,7 @@ namespace Game.Mecanics
 
             IsAttacking = false;
             WeaponTarget = null;
+            _attackedEnemies.Clear();
         }
 
         public override void Attack(Character target = null)
@@ -132,7 +146,7 @@ namespace Game.Mecanics
             }
 
             base.Attack(WeaponTarget);
-        
+
             StartCoroutine(DisableAttackDeleyed());
         }
     }
