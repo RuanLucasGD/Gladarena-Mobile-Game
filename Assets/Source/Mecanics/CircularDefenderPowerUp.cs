92cee8da3ec0d0ecc7a4e50d40fb9861a51b6864
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2023-04-08 13:37:54 -0400

    implement: add timers to circular power up

--- Assets/Source/Mecanics/CircularDefenderPowerUp.cs
+++ Assets/Source/Mecanics/CircularDefenderPowerUp.cs
@@ -6,13 +6,37 @@ namespace Game.Mecanics
 {
     public class CircularDefenderPowerUp : PowerUp
     {
+        [Header("Behaviour")]
         public float TurnSpeed;
         public float Force;
 
+        [Header("Timers")]
+        public float DisarmAfterTime;
+        public float RearmAfterTime;
+
+        public GameObject Model;
+
         private float _currentRot;
+
+        public bool PowerUpEnabled { get; private set; }
+
+        public override Character Owner
+        {
+            get => base.Owner;
+            set
+            {
+                base.Owner = value;
+                EnablePowerUp();
+            }
+        }
 
         void Update()
         {
+            if (!PowerUpEnabled)
+            {
+                return;
+            }
+
             _currentRot += TurnSpeed * Time.deltaTime;
             if (_currentRot > 360) _currentRot -= 360;
             if (_currentRot < 0) _currentRot += 360;
@@ -20,8 +44,26 @@ namespace Game.Mecanics
             transform.rotation = Quaternion.Euler(Vector3.up * _currentRot);
         }
 
+        private void EnablePowerUp()
+        {
+            PowerUpEnabled = true;
+            Model.SetActive(true);
+            Invoke(nameof(DisablePowerUp), DisarmAfterTime);
+        }
+
+        private void DisablePowerUp()
+        {
+            PowerUpEnabled = false;
+            Model.SetActive(false);
+            Invoke(nameof(EnablePowerUp), RearmAfterTime);
+        }
+
         private void OnTriggerEnter(Collider other)
         {
+            if (!PowerUpEnabled)
+            {
+                return;
+            }
             if (other.TryGetComponent<Character>(out var c))
             {
                 if (c != Owner)
